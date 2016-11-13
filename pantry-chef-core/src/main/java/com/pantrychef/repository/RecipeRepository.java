1aea96594fc4b2ffa2f78de8ec8872044de53524
guilherme platzeck <guilherme.platzeck@ifood.com.br>
2016-11-13 03:00:19 -0200

    unused class, missing reposiroty annotation

--- pantry-chef-core/src/main/java/com/pantrychef/repository/RecipeRepository.java
+++ pantry-chef-core/src/main/java/com/pantrychef/repository/RecipeRepository.java
@@ -3,9 +3,11 @@ package com.pantrychef.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pantrychef.model.RecipeTO;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<RecipeTO, UUID> {
 
 }
